458d7f794776fa70c8e2453fb84026ac5a55602c
vimal patel <vimal.patel.nvs@gmail.com>
2020-05-02 18:34:42 +0530

    Added fevicon icon and header style changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import LoanForm from './components/form/LoanForm';
 import LoanReport from './components/report/LoanReport';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { StylesProvider } from '@material-ui/core/styles';
-import {calcHomeLoan} from './shared/calculate-service';
+import { calcHomeLoan } from './shared/calculate-service';
 import Grid from '@material-ui/core/Grid';
 import './assets/App.scss';
+import appIcon from './assets/appIcon.png';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class App extends Component {
-
-	constructor() {
-		super();
-		this.state = {
-			showResult:false
-		};
+const useStyles = makeStyles({
+	root: {
+		background: '#3d4977'
+	},
+	headerIcon: {
+		margin: '0 10px'
 	}
+});
 
-	onFormSubmit = (loanDetails) => {
-		const {loanAmount,emi,interestRate,prePayment }= loanDetails;
-        var result = calcHomeLoan(loanAmount,emi,interestRate,prePayment);
-     
+const Header = () => {
+	const classes = useStyles();
+	return (
+		<AppBar position="static" color="primary" elevation={0} className={classes.root}>
+			<Toolbar>
+				<img src={appIcon} className={classes.headerIcon}></img>
+				<Typography variant="h6" color="inherit" noWrap>
+					Home Loan Calculator
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	)
+}
+
+const App = () => {
+
+	const onFormSubmit = (loanDetails) => {
+		const { loanAmount, emi, interestRate, prePayment } = loanDetails;
+		var result = calcHomeLoan(loanAmount, emi, interestRate, prePayment);
+
 		this.setState({
 			showResult: true,
-			loanScheduleResult : result
+			loanScheduleResult: result
 		});
 	}
 
-	render() {
-		return (
-			<React.Fragment>
-				<StylesProvider injectFirst>
-				<AppBar position="static" color="primary" elevation={0} >
-					<Toolbar>
-						<Typography variant="h6" color="inherit" noWrap>
-							Home Loan Calculator
-						</Typography>
-					</Toolbar>
-				</AppBar>
+	return (
+		<React.Fragment>
+			<StylesProvider injectFirst>
+				<Header/>
 				{/* <Grid container spacing={0} className="grid-container">
                     <Grid item xs={12} md={4} className="form">
 						<LoanForm onSubmit={this.onFormSubmit} />
@@ -47,9 +58,10 @@ export default class App extends Component {
 						{this.state.showResult ? <LoanReport result={this.state.loanScheduleResult}/> : null}
 					</Grid>
 				</Grid> */}
-				</StylesProvider>
-			</React.Fragment >
-		)
-	}
+			</StylesProvider>
+		</React.Fragment >
+	)
 
 }
+
+export default App;
