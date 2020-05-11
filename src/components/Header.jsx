624a18094e079ae90fff50a14923dc897c55ee49
vimal patel <vimal.patel.nvs@gmail.com>
2020-05-11 10:26:14 +0530

    Fixed: home page redirection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,7 @@ import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Link from 'next/link';
-
-const useStyles = makeStyles({
-	root: {
-		background: '#f7f7eb',
-		color: 'rgba(0, 0, 0, 0.87)',
-		boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
-	},
-	headerIcon: {
-		margin: '0 10px'
-	},
-	link: {
-		color: '#fff',
-		textDecoration: 'initial'
-	}
-});
 
 const Header = () => {
 	return (
@@ -26,8 +10,8 @@ const Header = () => {
 			<Toolbar>
 					<img src='./appIcon.png' className='headerIcon'></img>
 					<Typography variant="h6" color="inherit" noWrap>
-						<Link href="/about" >
-          					<a className='link'>	Home Loan Calculator</a>
+						<Link href="/" >
+          					<a className='link'>Home Loan Calculator</a>
         				</Link>
 					</Typography>
 			</Toolbar>
