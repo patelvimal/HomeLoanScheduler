87b72653342068eaf17bdabeb7c0f9d7f348004f
vimal patel <vimal.patel.nvs@gmail.com>
2020-05-18 10:11:16 +0530

    Fixed: page flicker issues due to css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,19 @@
 import App from 'next/app'
 import Head from 'next/head'
 import React from 'react'
-import '../assets/App.scss';
-import '../assets/InputSlider.scss';
+// import '../assets/fonts.css';
+// import '../assets/site.css';
+// import '../assets/App.scss';
+// import '../assets/InputSlider.scss';
 import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { StylesProvider } from '@material-ui/core/styles';
 import Header from '../components/Header';
 
-const theme = createMuiTheme({
-	typography: {
-		fontFamily: "Roboto-Regular"
-	}
-});
+// const theme = createMuiTheme({
+// 	typography: {
+//     // fontFamily: "Roboto-Regular"
+// 	}
+// });
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
