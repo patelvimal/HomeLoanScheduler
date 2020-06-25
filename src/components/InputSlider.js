9ee023f28806a9165499f41b114f81bca6582e02
vimal patel <vimal.patel.nvs@gmail.com>
2020-06-25 16:11:49 +0530

    input slider enhancement

--- src/components/InputSlider.js
+++ src/components/InputSlider.js
@@ -9,14 +9,13 @@ const InputSlider = props => {
   const decimalRegex = /^((?:|1|[1-9]\d?|100)(?:\.\d{0,2})?)$/;
   const regex = props.type == 'Decimal' ? decimalRegex : integerRegex;
 
-  const handleSliderChange = (event, newValue) => {
-    setValue(newValue);
-    invokeCallback(newValue);
+  const handleSliderChange = value => {
+    setValue(value);
+    invokeCallback(value);
   };
 
-  const handleInputChange = event => {
-	const {value} = event.target;
-	setValue('asdasdasdasdasdasd');
+  const handleInputChange = textValue => {
+    setValue(textValue);
     // if (value === '' || (regex.test(value) && (value >= min && value <= max))) {
     //   const newValue =
     //     value === ''
@@ -39,30 +38,74 @@ const InputSlider = props => {
     <React.Fragment>
       <View style={styles.root}>
         <Text style={styles.label}>{props.label}</Text>
-        <Input placeholder="" type="number" onChange={handleInputChange} style={styles.input} />
+        <Input
+          value={value}
+          placeholder=""
+          keyboardType="numeric"
+          onChangeText={handleInputChange}
+          style={styles.input}
+        />
       </View>
-      <View>
-        <Slider value={50} onValueChange={value => setValue({value})} />
-        <Text>hello: {value}</Text>
+      <View style={styles.sliderRoot}>
+        <Slider
+          value={value}
+          onValueChange={handleSliderChange}
+          minimumValue={min}
+          maximumValue={max}
+          step={step}
+          trackStyle={styles.track}
+          slider={styles.slider}
+          thumbTouchSize={styles.thumb}
+          thumbTintColor='#fff'
+          thumbStyle={styles.thumbStyle}
+        />
+        <Text>hellos: {value}</Text>
       </View>
     </React.Fragment>
   );
 };
 
 const styles = StyleSheet.create({
-	root:{
-		flexDirection:'row'
-	},
-	label: {
-		padding: '6px 0',
-		marginTop: 4,
-		fontSize: 20
-	},
-	input: {
-		padding: '8px',
-		fontSize: 18,
-		width: 80,
-	}
+  root: {
+    flexDirection: 'row',
+  },
+  label: {
+    padding: 6,
+    marginTop: 4,
+    fontSize: 20,
+  },
+  input: {
+    padding: 8,
+    fontSize: 18,
+    width: 8,
+  },
+  sliderRoot: {
+    padding: 6,
+  },
+  track: {
+    padding:4,
+    borderRadius:50,
+    borderColor:'red',
+    borderWidth:5,
+    backgroundColor:'#fff',
+    color: '#fff'
+  },
+  slider:{
+    backgroundColor:'darkslateblue',
+    color:'darkslateblue'
+  },
+  thumb:{
+    width: 4000, height: 4000
+  },
+  thumbStyle : {
+    backgroundColor:'#fff',
+    padding:12,
+    borderStyle:'solid',
+    borderColor:'#4d4d4d',
+    borderWidth:2,
+    marginTop:2,
+    borderRadius:100
+  }
 });
 
 export default InputSlider;
--- src/components/LoanForm.js
+++ src/components/LoanForm.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import InputSlider from './InputSlider'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import InputSlider from './InputSlider';
 
 const LoanForm = () => {
-    return (
-        <View>
-            <InputSlider label="Outstanding Loan Amount"/>
-        </View>
-    )
+  const LOAN_AMOUNT_DEFAULT_VALUE = 1;
+  const MONTHLY_EMI_DEFAULT_VALUE = 50;
+  const INTEREST_RATE_DEFAULT_VALUE = 9;
+  const LOAN_TENURE_DEFAULT_VALUE = 1;
+  const MONTHLY_PREPAYMENT_DEFAULT_VALUE = 0;
+
+  const onChange = (name, newValue) => {
+    // setLoanInfo(prevState => ({
+    //     ...prevState,
+    //     [name]: newValue
+    // }))
 }
 
-const styles = StyleSheet.create({})
+
+  return (
+    <View>
+      <InputSlider
+        label="Outstanding Loan Amount"
+        min={0}
+        max={100}
+        step={1}
+        defaultValue={LOAN_AMOUNT_DEFAULT_VALUE}
+        onChange={onChange}
+        suffix="L"
+        name="loanAmount"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default LoanForm;
-
-
