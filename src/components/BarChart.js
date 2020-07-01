ca60946e219c0c8e55def727a4b6f6b99052457b
vimal patel <vimal.patel.nvs@gmail.com>
2020-07-01 23:46:30 +0530

    Chart Enhancement

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -5,38 +5,31 @@ import {
   VictoryBar,
   VictoryTheme,
   VictoryAxis,
-  VictoryGroup
+  VictoryGroup,
 } from 'victory-native';
 
 const BarChart = props => {
   return (
     <VictoryChart theme={VictoryTheme.material}>
-      <VictoryGroup offset={17} colorScale={["#08a308", "#ad0f98"]}>
-        <VictoryBar
-          x="year"
-          y="principal"
-          barWidth={10}
-          data={props.data}
-        />
-        <VictoryBar
-          x="year"
-          y="interest"
-          barWidth={10}
-          data={props.data}
-        />
+      <VictoryGroup
+        offset={10}
+        colorScale={['#08a308', '#ad0f98']}
+       >
+        <VictoryBar x="year" y="principal" barWidth={10} data={props.data} />
+        <VictoryBar x="year" y="interest" barWidth={10} data={props.data} />
       </VictoryGroup>
 
       <VictoryAxis
         label="Year"
         fixLabelOverlap
-        style={{axisLabel: {padding: 35,fontSize:15 }}}
+        style={{axisLabel: {padding: 35, fontSize: 15}}}
       />
       <VictoryAxis
         dependentAxis
         label="Principal/Interest"
         tickFormat={x => `${x / 100000}L`}
         style={{
-          axisLabel: {padding: 33,fontSize:15 },
+          axisLabel: {padding: 33, fontSize: 15},
         }}
       />
     </VictoryChart>
--- src/components/LineCharts.js
+++ src/components/LineCharts.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import {Text} from 'react-native';
-import {VictoryChart, VictoryLine, VictoryTheme,VictoryAxis} from 'victory-native';
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+  VictoryAxis,
+  VictoryScatter,
+} from 'victory-native';
 const Chart = props => {
   return (
-    //<Text>{JSON.stringify(props.data)}</Text>
-    // <PureChart data={props.data} type='line' />
     <VictoryChart theme={VictoryTheme.material}>
       <VictoryLine
         style={{
-          data: {stroke: '#08a308',
-                strokeWidth: 4, strokeLinecap: "round" ,
-            }
+          data: {stroke: '#08a308', strokeWidth: 4, strokeLinecap: 'round'},
         }}
         x="year"
-        y='principal'
+        y="principal"
         data={props.data}
+      />
+       <VictoryScatter
+        data={props.data}
+        x="year"
+        y="principal"
+        size={5}
+        style={{data: {fill: '#08a308'}}}
       />
       <VictoryLine
         style={{
-            data: {stroke: '#ad0f98',
-            strokeWidth: 4, strokeLinecap: "round" ,
-          },
+          data: {stroke: '#ad0f98', strokeWidth: 4, strokeLinecap: 'round'},
           parent: {border: '50px solid #ccc'},
         }}
         x="year"
-        y='interest'
+        y="interest"
         data={props.data}
       />
-    <VictoryAxis
-            label="Year"
-            fixLabelOverlap
-            style={{axisLabel: { padding: 35,fontSize:15  }}}
-    />
-    <VictoryAxis dependentAxis
-    label="Principal/Interest"
-        tickFormat={(x) => (`${x / 100000}L`)}
-      style={{
-        axisLabel: { padding: 33, fontSize:15 }
-      }}
-    />
+      <VictoryScatter
+        data={props.data}
+        x="year"
+        size={5}
+        style={{data: {fill: '#ad0f98'}}}
+        y="interest"
+      />
+      <VictoryAxis
+        label="Year"
+        fixLabelOverlap
+        style={{axisLabel: {padding: 35, fontSize: 15}}}
+      />
+      <VictoryAxis
+        dependentAxis
+        label="Principal/Interest"
+        tickFormat={x => `${x / 100000}L`}
+        style={{
+          axisLabel: {padding: 33, fontSize: 15},
+        }}
+      />
     </VictoryChart>
   );
 };
