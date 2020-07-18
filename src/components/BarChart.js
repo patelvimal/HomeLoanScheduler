e3b8bb58be0265a22cc9a848b410cadfafc40a29
vimal patel <vimal.patel.nvs@gmail.com>
2020-07-18 14:12:26 +0530

    Bar chart UI enhancement

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import {
   VictoryChart,
   VictoryBar,
@@ -12,38 +12,52 @@ import {
 const BarChart = props => {
   return (
     <VictoryChart
-      theme={VictoryTheme.material}
-      padding={{top: 10, left: 40, right: 10, bottom: 50}}>
+      theme={VictoryTheme.grayscale}
+      padding={{ top: 10, left: 40, right: 10, bottom: 50 }}>
       <VictoryGroup offset={10} colorScale={['#08a308', '#ad0f98']}>
-        <VictoryBar x="year" y="principal" barWidth={10} data={props.data} />
-        <VictoryBar x="year" y="interest" barWidth={10} data={props.data} />
+        <VictoryBar x="year" y="principal" barWidth={10} data={props.data}
+          style={{
+            data: {
+              fill: "#08a308",
+              stroke: "#000000",
+              strokeWidth: 1
+            }
+          }}
+        />
+        <VictoryBar x="year" y="interest" barWidth={10} data={props.data} 
+          style={{
+            data: {
+              fill: "#ad0f98",
+              stroke: "#000000",
+              strokeWidth: 1
+            }
+          }}
+        />
       </VictoryGroup>
       <VictoryLegend
         x={70}
         y={10}
         orientation="horizontal"
         gutter={0}
-        //style={{border: {stroke: 'black'}}}
-        //style={{border: {stroke: 'black'}}}
         data={[
           {
             name: 'Principal',
-            symbol: {fill: '#08a308'},
-            labels: {fontSize: 15},
+            symbol: { fill: '#08a308' },
+            labels: { fontSize: 15 },
           },
-          {name: 'Interest', symbol: {fill: '#ad0f98'}, labels: {fontSize: 15}},
+          { name: 'Interest', symbol: { fill: '#ad0f98' }, labels: { fontSize: 15 } },
         ]}
       />
       <VictoryAxis
         label="Year"
         fixLabelOverlap
-        style={{axisLabel: {padding: 35, fontSize: 15}}}
+        style={{ axisLabel: { padding: 35, fontSize: 15 } }}
       />
       <VictoryAxis
         dependentAxis
         tickFormat={x => `${x / 100000}L`}
         style={{
-          axisLabel: {padding: 33, fontSize: 15},
+          axisLabel: { padding: 33, fontSize: 15 },
         }}
       />
     </VictoryChart>
