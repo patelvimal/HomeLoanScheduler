244f7380cfe745d09315ca8fdd3d430afca21f38
vimal patel <vimal.patel.nvs@gmail.com>
2020-07-22 23:40:06 +0530

    Ui design changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Card} from 'react-native-elements';
-import {StyleSheet, View} from 'react-native';
+import { Card } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
 
 const CardLayout = props => {
   return (
     <View style={style.root}>
       <Card
         title={props.title}
-        containerStyle={style.container}
+        titleStyle={style.title}
         dividerStyle={style.divider}
-        wrapperStyle={style.root}
-        titleStyle={style.title}>
+        containerStyle={style.container}>
         <View
           style={props.removeContentStyle ? style.bottomMargin : style.content}>
           {props.children}
@@ -23,31 +22,43 @@ const CardLayout = props => {
 const style = StyleSheet.create({
   root: {
     zIndex: 0,
+  //  borderWidth: 2,
+    borderColor: 'red',
+//    marginTop: 20,
+    marginBottom: 0,
+  elevation:6,
+    margin:0
   },
   title: {
     textTransform: 'capitalize',
-    backgroundColor: '#fff',
-    borderBottomColor: '#b2b1a9',
-    borderBottomWidth: 2,
-    borderStyle: 'dashed',
-    color:'grey',
-    padding: 12,
-    margin: 0,
-    fontSize: 17,
+    color: 'grey',
+    paddingTop: 12,
+    fontSize: 16,
+    fontWeight:'400',
+    textAlign:'left',
+    letterSpacing:1,
+    //marginTop:-14,
+    //borderWidth:2,
+    //borderBottomWidth:1,
+    borderBottomColor:'#ccc7c7',
+    paddingLeft:16,
+    backgroundColor:'#fff',
   },
   container: {
     padding: 0,
     margin: 0,
     // TO get rid of border of card uncommment below 2 lines
-    elevation:0,
-    backgroundColor:'#fff'
+    elevation: 0,
+    borderWidth:0,
+    backgroundColor: '#fff'
   },
   content: {
-    marginLeft: 20,
-    marginRight: 20,
-    marginBottom: 20,
-    borderWidth: 0,
-    borderColor: 'red',
+    // marginLeft: 20,
+    // marginRight: 20,
+    // marginBottom: 20,
+    borderWidth:0,
+    padding: 20,
+    //    backgroundColor:'blue',
   },
   bottomMargin: {
     marginBottom: 25,
--- src/components/SummaryReport.js
+++ src/components/SummaryReport.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {ListItem, PricingCard} from 'react-native-elements';
 import RupeeIcon from './RupeeIcon';
+import HighlightedCard from './HighlighedCard';
 
 const SummaryReport = props => {
   const {total, completionDate, principal, interest, emi} = props.data;
   return (
     <View>
-      <ListItem
-        title="Monthly EMI"
-        rightTitle={emi.addThousandSeperator()}
-        rightIcon={<RupeeIcon style={styles.rupeeIcon} />}
-        containerStyle={styles.highlight}
-        rightTitleStyle={styles.labelValue}
-      />
-      <ListItem
-        title="Completion Date"
-        rightTitle={completionDate}
-        containerStyle={styles.highlight}
-        rightTitleStyle={styles.labelValue}
-      />
+      <View style={styles.emiInfo}>
+        <Text style={styles.emiLabel}>Monthly EMI</Text>
+        <View style={styles.emiValue}>
+            <RupeeIcon style={styles.rupeeIconBig}/>
+            <Text style={styles.emiValueText}>{emi.addThousandSeperator()}</Text>
+        </View>
+      </View>
+      <View style={styles.completionInfo}>
+        <Text style={styles.completionLabel}>Completion Date</Text>
+        <View style={styles.completionValue}>
+            <Text style={styles.completionValueText}>{completionDate}</Text>
+        </View>
+      </View>
       <ListItem
         title="Total Interest"
         rightTitle={interest.addThousandSeperator()}
@@ -38,20 +39,69 @@ const SummaryReport = props => {
 };
 
 const styles = StyleSheet.create({
-    highlight:{
-        backgroundColor: '#ffeb3b8f',
-        borderRadius: 10,
-        marginBottom:10,
-    },
-    labelValue:{
-        color: '#545454',
-        fontWeight:'bold',
-        alignItems:'flex-end',
-    },
-    rupeeIcon: {
-        fontSize:22,
-        marginTop:0,
-        color:'grey',
-    }
+  highlight: {
+    backgroundColor: '#ffeb3b8f',
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  labelValue: {
+    color: '#545454',
+    fontWeight: 'bold',
+    alignItems: 'flex-end',
+  },
+  rupeeIcon: {
+    fontSize: 22,
+    marginTop: 0,
+    color: 'grey',
+  },
+  emiLabel: {
+    fontSize: 22,
+    color:'forestgreen',
+    fontWeight:'bold'
+  },
+  rupeeIconBig: {
+    fontSize:43,
+    paddingRight:15,
+    color:'darkmagenta',
+  },
+  emiValue: {
+    flexDirection:'row',
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  emiValueText : {
+    fontSize:45,
+    color:'darkmagenta',
+    fontWeight:'bold'
+  },
+  emiInfo : {
+    // flexDirection:'row',
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom:20,
+    backgroundColor: '#ffeb3b8f',
+    borderRadius: 10,
+    padding:10
+  },
+  completionLabel: {
+    fontSize: 20,
+    color:'forestgreen',
+    fontWeight:'bold'
+  },
+  completionInfo : {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom:20,
+    elevation:0,
+    borderWidth:0
+  },
+  completionValueText : {
+    fontSize:40,
+    color:'darkmagenta',
+    fontWeight:'bold'
+  },
 })
 export default SummaryReport;
